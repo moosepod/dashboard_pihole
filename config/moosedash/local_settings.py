de191ea4518f7fac7de8d96542056fefbda9b540
Matthew Christensen <mchristensen@moosepod.com>
2021-02-07 15:22:36 -0500

    Added config

---
+++ config/moosedash/local_settings.py
@@ -0,0 +1,7 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': '/usr/local/django/moosedash/moosedash.sqlite3'
+    }
+}
+ALLOWED_HOSTS=['pi.hole','192.168.0.13','192.168.1.23','192.168.1.19','dashboard.thechristensens.org']
